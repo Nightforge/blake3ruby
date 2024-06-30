b25adae69a8f28659e09f3b82605a460ff5b62bd
Evgeniy <modo00@gmail.com>
2024-06-30 17:57:40 +0300

    Enhance benchmarking script to test various message lengths for hashing algorithms

--- spec/benchmark.rb
+++ spec/benchmark.rb
@@ -4,16 +4,23 @@ require "digest"
 require "blake3ruby"
 require "benchmark"
 
-message = "This is a message to be hashed"
+messages = [
+  "Short message",
+  "This is a medium length message to be hashed",
+  "This is a much longer message designed to see how the algorithms perform when dealing with a significantly larger amount of data, which should provide a better overall benchmark of their performance."
+]
 
 n = 5_000_000
 
-Benchmark.bm(8) do |x|
-  x.report("MD5:") { n.times { Digest::MD5.hexdigest(message) } }
-  x.report("SHA1:") { n.times { Digest::SHA1.hexdigest(message) } }
-  x.report("SHA3-256:") { n.times { Digest::SHA256.hexdigest(message) } }
-  x.report("SHA3-384:") { n.times { Digest::SHA384.hexdigest(message) } }
-  x.report("SHA3-512:") { n.times { Digest::SHA512.hexdigest(message) } }
-  x.report("RMD160:") { n.times { Digest::RMD160.hexdigest(message) } }
-  x.report("blake3:") { n.times { Blake3ruby.hexdigest(message) } }
+Benchmark.bm(20) do |x|
+  messages.each do |message|
+    length = message.length
+    x.report("MD5 (#{length} chars):") { n.times { Digest::MD5.hexdigest(message) } }
+    x.report("SHA1 (#{length} chars):") { n.times { Digest::SHA1.hexdigest(message) } }
+    x.report("SHA3-256 (#{length} chars):") { n.times { Digest::SHA256.hexdigest(message) } }
+    x.report("SHA3-384 (#{length} chars):") { n.times { Digest::SHA384.hexdigest(message) } }
+    x.report("SHA3-512 (#{length} chars):") { n.times { Digest::SHA512.hexdigest(message) } }
+    x.report("RMD160 (#{length} chars):") { n.times { Digest::RMD160.hexdigest(message) } }
+    x.report("blake3 (#{length} chars):") { n.times { Blake3ruby.hexdigest(message) } }
+  end
 end
